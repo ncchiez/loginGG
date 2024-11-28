9d44843fbbd7998af17da064ae91fcf63ab16058
ncchiez <chiennc09@gmail.com>
2024-11-28 22:56:55 +0700

    ko biet commit gi

---
+++ src/configurations/configuration.js
@@ -0,0 +1,5 @@
+export const OAuthConfig = {
+  clientId: "333310659062-eoevepvn0ivn4i8f6rhjr9q1mrgsrb2j.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
